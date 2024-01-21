67a250d88c3bbe1c478bab20d416487301afba6f
ngjiekang <64952428+ngjiekang@users.noreply.github.com>
2024-01-22 01:32:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,34 +6,34 @@ app = Flask(__name__)
 class User(models.Model):
     pass
 
-@app.route("/users/<username>")
-def show_user(username):
-    with connection.cursor() as cursor:
-        # GOOD -- Using parameters
-        cursor.execute("SELECT * FROM users WHERE username = %s", username)
-        User.objects.raw("SELECT * FROM users WHERE username = %s", (username,))
+# @app.route("/users/<username>")
+# def show_user(username):
+#     with connection.cursor() as cursor:
+#         # GOOD -- Using parameters
+#         cursor.execute("SELECT * FROM users WHERE username = %s", username)
+#         User.objects.raw("SELECT * FROM users WHERE username = %s", (username,))
 
-        # BAD -- Using string formatting
-        cursor.execute("SELECT * FROM users WHERE username = '%s'" % username)
+#         # BAD -- Using string formatting
+#         cursor.execute("SELECT * FROM users WHERE username = '%s'" % username)
 
-        # BAD -- other ways of executing raw SQL code with string interpolation
-        User.objects.annotate(RawSQL("insert into names_file ('name') values ('%s')" % username))
-        User.objects.raw("insert into names_file ('name') values ('%s')" % username)
-        User.objects.extra("insert into names_file ('name') values ('%s')" % username)
+#         # BAD -- other ways of executing raw SQL code with string interpolation
+#         User.objects.annotate(RawSQL("insert into names_file ('name') values ('%s')" % username))
+#         User.objects.raw("insert into names_file ('name') values ('%s')" % username)
+#         User.objects.extra("insert into names_file ('name') values ('%s')" % username)
 
-        # BAD (but currently no custom query to find this)
-        #
-        # It is exposed to SQL injection (https://docs.djangoproject.com/en/2.2/ref/models/querysets/#extra)
-        # For example, using name = "; DROP ALL TABLES -- "
-        # will result in SQL: SELECT * FROM name WHERE name = ''; DROP ALL TABLES -- ''
-        #
-        # This shouldn't be very widespread, since using a normal string will result in invalid SQL
-        # Using name = "example", will result in SQL: SELECT * FROM name WHERE name = ''example''
-        # which in MySQL will give a syntax error
-        #
-        # When testing this out locally, none of the queries worked against SQLite3, but I could use
-        # the SQL injection against MySQL.
-        User.objects.raw("SELECT * FROM users WHERE username = '%s'", (username,))
+#         # BAD (but currently no custom query to find this)
+#         #
+#         # It is exposed to SQL injection (https://docs.djangoproject.com/en/2.2/ref/models/querysets/#extra)
+#         # For example, using name = "; DROP ALL TABLES -- "
+#         # will result in SQL: SELECT * FROM name WHERE name = ''; DROP ALL TABLES -- ''
+#         #
+#         # This shouldn't be very widespread, since using a normal string will result in invalid SQL
+#         # Using name = "example", will result in SQL: SELECT * FROM name WHERE name = ''example''
+#         # which in MySQL will give a syntax error
+#         #
+#         # When testing this out locally, none of the queries worked against SQLite3, but I could use
+#         # the SQL injection against MySQL.
+#         User.objects.raw("SELECT * FROM users WHERE username = '%s'", (username,))
 
 
 def imagemath_eval(expression, _dict={}, **kw):
@@ -55,13 +55,10 @@ def imagemath_eval(expression, _dict={}, **kw):
     args.update(kw)
     for k, v in list(args.items()):
         if hasattr(v, "im"):
-            args[k] = _Operand(v)
+            pass
 
     out = builtins.eval(expression, args)
-    try:
-        return out.im
-    except AttributeError:
-        return out
+    
 
 from PIL import Image, ImageMath
 @app.route("/files/<expression>")
